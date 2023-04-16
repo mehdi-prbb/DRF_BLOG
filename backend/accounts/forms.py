f3045d51e5e0d803ab7b53b6ced0ce04ea6a5eeb
mehdi pourbaba <mpourbaba373@gmail.com>
2023-04-16 20:23:16 +0330

    create CustomUserCreationForm and CustomUserChaangeForm

---
+++ backend/accounts/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from . models import CustomUser
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='password confirm', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'phone_number', 'full_name')
+
+    def clean_password2(self):
+        cd = self.changed_data
+
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise ValidationError('passwords dont match')
+        return cd['password2']
+    
+    def save(self, commit:True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+    
+
+class CustomUserChaangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(
+        help_text='you can change password from this <a href="../password/">link</a>'
+        )
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'phone_number', 'full_name', 'password', 'last_login')
