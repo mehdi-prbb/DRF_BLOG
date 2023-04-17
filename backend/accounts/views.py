0ec06f214a73ddea1c00a21b5eb33501cbd67686
mehdi pourbaba <mpourbaba373@gmail.com>
2023-04-17 23:28:04 +0330

    set UserRegister to accounts view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import UserRegisterSerializer
+
+class UserRegister(APIView):
+    """
+    Register new users using phone number or email and password.
+    """
+
+    def post(self, request):
+        ser_data = UserRegisterSerializer(data=request.data)
+        
+        if ser_data.is_valid():
+            ser_data.create(ser_data.validated_data)
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
